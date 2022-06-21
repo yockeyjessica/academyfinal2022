130b941a35724c608003c8dab0ea1700f700d87a
John Lange <lange.john.m@gmail.com>
2022-06-21 10:18:35 -0400

    Api Example

---
+++ MvcTourney3/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcTourney3.Models;
+using MvcTourney3.Data;
+
+namespace MvcTourney3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly MvcTourney3Context _context;
+
+        public ValuesController(MvcTourney3Context context)
+        {
+            _context = context;
+        }
+
+        [Route("getteams")]
+        public List<Team> GetTeams()
+        {
+            return _context.Team.ToList();
+        }
+    }
+}
